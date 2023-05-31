65812244aae8ad6d4a73fefe21eebdb5099fac04
Naprani <109498379+Naprani@users.noreply.github.com>
2023-05-31 14:39:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,25 +1,25 @@
-const fs = require('fs')
+const  fs  =  membutuhkan ( 'fs' )
 
 // Api
 global.APIs = {
 	alfa: 'https://api.zeeoneofc.my.id',
 }
 
-// Apikey
+// Kunci API
 global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global . namabot        =       "RanBot"
+global . namapemilik    =     "Ranran"
+global . nama  paket      =       namabot
+global . pengarang  =  namapemilik
+global . myweb  =  "https://s.id/zeeoneofc"
+global . footer_text  =  "© "  +  namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
-global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.sessionName = 'session'
+
+global . pemilik        = [ '6287877704607' ]
+global . namasesi  =  'sesi'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
 global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
